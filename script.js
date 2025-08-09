82876e0e5d9d3a38a5868830e6dd6884922b2905
Rishabh Dhoundiyal <165043134+RDESTROYER1@users.noreply.github.com>
2025-08-09 11:48:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Discover', 'Invest', 'Thrive'],
+    typeSpeed: 50,
+});
+
+
+document.getElementById("githubBtn").addEventListener("click", function () {
+    window.open("https://www.chicago.gov/content/dam/city/sites/invest_sw/rfp/11339-43_s_michigan_appraisal.pdf", "_blank");
+});
