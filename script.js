110343b08bd707c3e0fbe1345bb282cb38fe5add
Rishabh Dhoundiyal <165043134+RDESTROYER1@users.noreply.github.com>
2025-08-21 11:53:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function doSearch() {
     const query = (input?.value || "").toLowerCase().trim();
 
-    fetch("fetch("https://rdestroyer1.github.io/Real-estate-database-by-Rishabh-Dhoundiyal/database.json")
+    fetch("https://rdestroyer1.github.io/Real-estate-database/database.json")
       .then((r) => r.json())
       .then((data) => {
         const locations = data.locations || [];
@@ -217,6 +217,4 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.key === "Enter") doSearch();
   });
 });
-
-
 
